950c2c7f704a994bf9246dfd3230393d380f8736
Will Albertsen <talktowillalbertsen@gmail.com>
2019-07-11 13:38:09 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,40 +1,8 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 const listItems = document.getElementsByClassName('student-item');
 const page = document.querySelector('.page');
 var isFloat = (listItems.length / 10) % 1 !== 0;
 
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 
 function showPage() {
     appendPageLinks();
@@ -74,9 +42,7 @@ function showPage() {
                 }
             }
             a[parseInt(deactivateQueue[0] - 1)].classList.remove('active');
-            deactivateQueue.shift();
-            /*console.log(deactivateQueue);*/
-            
+            deactivateQueue.shift(); 
             
         }
         
@@ -85,11 +51,6 @@ function showPage() {
 }
 
 showPage();
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
 
 
 function appendPageLinks() {
@@ -115,11 +76,3 @@ function appendPageLinks() {
         }
     }
 }
-
-
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
